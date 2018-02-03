f42127ecfdc544540f3841f3bb26b37b47d629dc
Nenad Djukic <Nenad2002>
2018-02-04 00:47:48 +0100

    creating variables

--- app/src/main/java/djuki/software/ch/dicee2/MainActivity.java
+++ app/src/main/java/djuki/software/ch/dicee2/MainActivity.java
@@ -1,8 +1,12 @@
 package djuki.software.ch.dicee2;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +16,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button rollButton;
-        
+        rollButton = (Button) findViewById(R.id.rollButton);
+
+        ImageView leftDice = (ImageView) findViewById(R.id.image_leftDice);
+
+        ImageView rightDice = (ImageView) findViewById(R.id.image_rightDice);
+
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Log.d("Dicee", "Ze Button has been pressed!");
+
+            }
+        });
+
+
     }
 }
