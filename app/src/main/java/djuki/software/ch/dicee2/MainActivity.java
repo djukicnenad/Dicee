9e81bb74a8c43d5aa96e10b67ef239c4276196b4
Nenad Djukic <Nenad2002>
2018-02-04 12:47:16 +0100

    random numbers

--- app/src/main/java/djuki/software/ch/dicee2/MainActivity.java
+++ app/src/main/java/djuki/software/ch/dicee2/MainActivity.java
@@ -20,9 +20,19 @@ public class MainActivity extends AppCompatActivity {
         Button rollButton;
         rollButton = (Button) findViewById(R.id.rollButton);
 
-        ImageView leftDice = (ImageView) findViewById(R.id.image_leftDice);
+        final ImageView leftDice = (ImageView) findViewById(R.id.image_leftDice);
 
-        ImageView rightDice = (ImageView) findViewById(R.id.image_rightDice);
+        final ImageView rightDice = (ImageView) findViewById(R.id.image_rightDice);
+
+        final int[] diceArray = {
+                    R.drawable.dice1,
+                    R.drawable.dice2,
+                    R.drawable.dice3,
+                    R.drawable.dice4,
+                    R.drawable.dice5,
+                    R.drawable.dice6
+
+        };
 
         rollButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,6 +45,14 @@ public class MainActivity extends AppCompatActivity {
                 int number = randomnumberGenerator.nextInt(6);
 
                 Log.d("Dicee", "The random number is:" + number);
+
+                leftDice.setImageResource(diceArray[number]);
+
+                number = randomnumberGenerator.nextInt(6);
+
+                rightDice.setImageResource(diceArray[number]);
+
+
 
 
 
